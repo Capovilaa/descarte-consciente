2c9f5e81342ade1769b6eba7f8c4238ce2506b25
Lucas Capovila <lcapovila@outlook.com>
2024-12-02 20:45:01 -0300

    added port to main file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -8,6 +9,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
