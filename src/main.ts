10146be77ff5b624bae73cdb434fdcf162bcc596
Lucas Capovila <lcapovila@outlook.com>
2024-12-02 21:11:42 -0300

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
